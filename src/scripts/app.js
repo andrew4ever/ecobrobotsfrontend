7c915866ce3f97e16ed5237270ee30bf6bd0141d
Andrew Yatsura <andrewyazura203@gmail.com>
2020-10-01 23:39:06 +0300

    Add custom marker icons for each AQI zone

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,58 +1,60 @@
-fetch('http://localhost:8080/map')
-  .then((response) => response.json())
-  .then((data) => {
-    if (!data.length) {
-      document.querySelector('div#aqi-general h2').innerHTML = 'N/A';
-      document.querySelector('div#aqi-general h5').innerHTML =
-        'No data available';
-      document.querySelector('span#latest-update').innerHTML =
-        'No data available';
-      return;
-    }
+function displayAqi() {
+  fetch('http://localhost:8080/map')
+    .then((response) => response.json())
+    .then((data) => {
+      if (!data.length) {
+        document.querySelector('div#aqi-general h1').innerHTML = 'N/A';
+        document.querySelector('div#aqi-general h4').innerHTML =
+          'No data available';
+        document.querySelector('span#latest-update').innerHTML =
+          'No data available';
+        return;
+      }
 
-    let point;
-    let average_aqi = 0;
-    let base_url = '';
+      let point;
+      let average_aqi = 0;
+      let base_url = 'https://i.imgur.com/';
 
-    for (point of data) {
-      average_aqi += point.aqi;
-      center = {
-        lat: parseFloat(point.latitude),
-        lng: parseFloat(point.longitude),
-      };
+      for (point of data) {
+        average_aqi += point.aqi;
+        center = {
+          lat: parseFloat(point.latitude),
+          lng: parseFloat(point.longitude),
+        };
 
-      icon = {
-        url: base_url + aqiMarkerColor(point.aqi),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(0, 10),
-        size: new google.maps.Size(20, 32),
-      };
+        icon = {
+          url: base_url + aqiMarkerColor(point.aqi),
+          origin: new google.maps.Point(0, -10),
+          anchor: new google.maps.Point(0, 0),
+          size: new google.maps.Size(27, 50),
+        };
 
-      new google.maps.Marker({
-        map,
-        // icon,
-        position: center,
-        animation: google.maps.Animation.DROP,
-        title: 'AQI',
-        label: {
-          text: point.aqi.toString(),
-          color: 'white',
-          fontFamily: 'Roboto',
-          fontWeight: 'bold',
-          fontSize: '14px',
-        },
-      });
-    }
+        new google.maps.Marker({
+          map,
+          icon,
+          position: center,
+          animation: google.maps.Animation.DROP,
+          title: 'AQI',
+          label: {
+            text: point.aqi.toString(),
+            color: 'black',
+            fontFamily: 'Roboto',
+            fontWeight: 'bold',
+            fontSize: '14px',
+          },
+        });
+      }
 
-    average_aqi /= data.length;
-    document.querySelector('div#aqi-general h1').innerHTML = Math.round(
-      average_aqi,
-    );
-    document.querySelector('div#aqi-general h4').innerHTML = aqiDescription(
-      average_aqi,
-    );
-    document.querySelector('span#latest-update').innerHTML = point.created;
-  });
+      average_aqi /= data.length;
+      document.querySelector('div#aqi-general h1').innerHTML = Math.round(
+        average_aqi,
+      );
+      document.querySelector('div#aqi-general h4').innerHTML = aqiDescription(
+        average_aqi,
+      );
+      document.querySelector('span#latest-update').innerHTML = point.created;
+    });
+}
 
 function aqiDescription(aqi) {
   if (aqi >= 0 && aqi <= 50) {
@@ -72,16 +74,24 @@ function aqiDescription(aqi) {
 
 function aqiMarkerColor(aqi) {
   if (aqi >= 0 && aqi <= 50) {
-    return 'marker-good.png';
+    // return 'marker-good.png';
+    return 'LLk8g21.png';
   } else if (aqi > 50 && aqi <= 100) {
-    return 'marker-moderate.png';
+    // return 'marker-moderate.png';
+    return '9FO3q7C.png';
   } else if (aqi > 100 && aqi <= 150) {
-    return 'marker-unhealthy-for-sensitive-groups.png';
+    // return 'marker-unhealthy-for-sensitive-groups.png';
+    return 'aCp0ABz.png';
   } else if (aqi > 150 && aqi <= 200) {
-    return 'marker-unhealthy.png';
+    // return 'marker-unhealthy.png';
+    return 'O5wvyYU.png';
   } else if (aqi > 200 && aqi <= 300) {
-    return 'marker-very-unhealthy.png';
+    // return 'marker-very-unhealthy.png';
+    return '3iSK85T.png';
   } else if (aqi > 300 && aqi <= 500) {
-    return 'marker-hazardous.png';
+    // return 'marker-hazardous.png';
+    return 'u3MyHA2.png';
   }
 }
+
+displayAqi();
